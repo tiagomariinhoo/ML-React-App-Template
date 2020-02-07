c3052147a50e8f51bc030f17dd3b10ba7bfe2b0e
Tiago Marinho <tiago.marinho.797@gmail.com>
2020-02-07 01:28:50 -0300

    default port

--- app.py
+++ app.py
@@ -114,5 +114,5 @@ class MainClass(Resource):
 
 
 if __name__ == "__main__":
-	port = int(os.environ.get('PORT', 33507))
-    app.run(host='0.0.0.0', port=port)
+	port = int(os.environ.get('PORT', 5000))
+	app.run(host='0.0.0.0', port=port)
