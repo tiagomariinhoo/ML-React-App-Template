531116301ca820d493f3940256cf18f21e09baf3
Tiago Marinho <tiago.marinho.797@gmail.com>
2020-02-07 00:08:02 -0300

    remove host

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ app = Api(app = flask_app,
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(port=port)
 
 name_space = app.namespace('prediction', description='Prediction APIs')
 
